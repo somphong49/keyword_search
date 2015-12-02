f5b61ba04e578006cb335417038d234a3604de21
somphong49 <diaw_10393_49@hotmail.com>
2015-12-02 19:05:38 +0700

    Finish without fixing

---
+++ keyword_search/BM-KMPClass.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keyword_search
+{
+    //boyer-moore-algorithm
+    public class Bm
+    {
+        public Kmp _kmp = new Kmp();
+        public int[] Compute_Last_Occurence_Function(string pattern)
+        {
+            const int sigma_size = 128;
+            int m = pattern.Length;
+            int[] lambda = new int[sigma_size];
+            for (int i = 0; i < lambda.Length; i++)
+            {
+                lambda[i] = -1;
+            }
+            for (int j = 0; j < m; j++)
+            {
+                lambda[(int)pattern[j]] = j;
+            }
+            return lambda;
+        }
+        public int[] Compute_Good_Suffix_Function(string pattern)
+        {
+            int m = pattern.Length;
+            var pi = _kmp.ComputePrefixFunction(pattern);
+            var P_0 = Reverse(pattern);
+            var pi_0 = _kmp.ComputePrefixFunction(P_0);
+            int[] gamma = new int[m + 1];
+            int j;
+            for (j = 0; j <= m; j++)
+            {
+                gamma[j] = (m - 1) - pi[m - 1];
+            }
+            for (int i = 0; i < m; i++)
+            {
+                j = (m - 1) - pi_0[i];
+                if (gamma[j] > i - pi_0[i])
+                {
+                    gamma[j] = i - pi_0[i];
+                }
+            }
+            return gamma;
+        }
+
+        private string Reverse(string text)
+        {
+            if (text == null)
+                return null;
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+
+        public int[] BM_Matcher(string Text, string Pattern)
+        {
+            int n = Text.Length;
+            int m = Pattern.Length;
+            int[] lambda = Compute_Last_Occurence_Function(Pattern);
+            int[] gamma = Compute_Good_Suffix_Function(Pattern);
+            int s = 0;
+            List<int> validshifts = new List<int>();
+
+            while (s <= n - m)
+            {
+                int j = m - 1;
+                while (j >= 0 && Pattern[j] == Text[s + j])
+                {
+                    j--;
+                }
+                if (j < 0)
+                {
+                    validshifts.Add(s);
+                    s = s + gamma[0];
+                }
+                else
+                {
+                    s = s + Math.Max(gamma[j], j - lambda[Text[s + j]]);
+                }
+            }
+            return validshifts.ToArray();
+        }
+    }
+
+    //Knuth-Morris-Pratt_Algorithm
+    public class Kmp
+    {
+        public void kmp_search(string P, string T)
+        {
+            int n = T.Length;
+            int m = P.Length;
+            int[] pi = ComputePrefixFunction(P);
+            int q = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                while (q > 0 && P[q] != T[i - 1])
+                {
+                    q = pi[q - 1];
+                }
+                if (P[q] == T[i - 1]) { q++; }
+                if (q == m)
+                {
+                    //Record a match was found here  
+                    q = pi[q - 1];
+                }
+            }
+        }
+
+        public int[] ComputePrefixFunction(string P)
+        {
+            int m = P.Length;
+            int[] pi = new int[m];
+            int k = 0;
+            pi[0] = 0;
+
+            for (int q = 1; q < m; q++)
+            {
+                while (k > 0 && P[k] != P[q]) { k = pi[k]; }
+
+                if (P[k] == P[q]) { k++; }
+                pi[q] = k;
+            }
+            return pi;
+        }
+    }
+
+
+}
---
+++ keyword_search/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace keyword_search
+{
+    class Program
+    {
+        static string[] InputProductData;
+        static List<Caldata> CalculateData = new List<Caldata>();
+        static string[] KeyPattern;
+        static Bm BoyerOperation = new Bm();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                readData();
+
+                readKeyPattern();
+
+                TextMatching();
+
+                SortingData();
+
+                ShowData();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error :"+ex.Message+Environment.NewLine+" Data:"+ex.Data);
+            }
+            Console.ReadKey();
+        }
+
+        static void readData()
+        {
+            string path = @"product.txt";
+
+            if(File.Exists(path))
+            {
+                InputProductData = File.ReadAllLines(path);
+            }
+            else 
+            {
+                Console.WriteLine("Error: There is no product data.");
+            }
+        }
+
+        static void readKeyPattern()
+        {
+            char[] delimiter = { ' ' };
+
+            Console.WriteLine("----Text Matching----");
+            Console.Write("Input Keyword :");
+            string input = Console.ReadLine();
+
+            KeyPattern = input.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        static void TextMatching()
+        {
+            if (InputProductData.Length != 0 && KeyPattern.Length != 0)
+            {
+
+                for (int i = 0; i < InputProductData.Length; i++)
+                {
+                    List<int> bmcaldata = new List<int>();
+                    for (int j = 0; j < KeyPattern.Length; j++)
+                    {
+                        int[] calbm = BoyerOperation.BM_Matcher(InputProductData[i].ToLower(), KeyPattern[j].ToLower());
+                        if (calbm.Length > 0)
+                        {
+                            bmcaldata.Add(calbm[0]);
+                        }
+                    }
+                    if(bmcaldata.Count >0)
+                        CalculateBMData(bmcaldata, i);
+                }
+            }
+        }
+
+        static void CalculateBMData(List<int> bmCal,int i)
+        {
+            Caldata x = new Caldata();
+            x.id = i;
+            x.MatchedNo = bmCal.Count;
+            x.FirstMatch = bmCal[0];
+            int matchdata = x.FirstMatch;
+            if (bmCal.Count > 1)
+            {
+                for (int j = 1; j < bmCal.Count; j++)
+                {
+                    matchdata = matchdata - bmCal[j];
+                }
+                x.Distance = Math.Abs(matchdata);
+            }
+
+            CalculateData.Add(x);
+        }
+
+        static void SortingData()
+        {
+            List<Caldata> TempList = new List<Caldata>();
+            List<int> SortMatchedNo = CalculateData.Select(i=>i.MatchedNo).Distinct().ToList();
+            for (int i = 0; i < SortMatchedNo.Count; i++)
+            {
+                List<Caldata> stSort = CalculateData.Where(a => a.MatchedNo == SortMatchedNo[i]).ToList();
+                List<int> ndMatch = stSort.Select(b => b.FirstMatch).Distinct().ToList();
+                for (int j = 0; j < ndMatch.Count; j++)
+                {
+                    List<Caldata> ndSort = stSort.Where(c => c.FirstMatch == ndMatch[j]).ToList();
+                    ndSort = ndSort.OrderBy(d => d.Distance).ToList();
+                    TempList.AddRange(ndSort);
+                }
+
+            }
+
+            CalculateData = TempList;
+
+        }
+
+        static void ShowData()
+        {
+            Console.WriteLine("Search Result is:" + Environment.NewLine);
+
+            foreach (Caldata item in CalculateData)
+            {
+                Console.WriteLine("- " + InputProductData[item.id]);
+            }
+
+            Console.WriteLine(Environment.NewLine+CalculateData.Count+" product(s) matched");
+        }
+    }
+
+    public class Caldata
+    {
+        public int id { get; set; }
+        public int MatchedNo { get; set; }
+        public int Distance { get; set; }
+        public int FirstMatch { get; set; }
+
+        public Caldata()
+        {
+            Distance = int.MaxValue;
+        }
+    }
+}
